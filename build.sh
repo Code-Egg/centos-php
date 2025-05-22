b206222a0ad7ad98be037e5cc256310eb808a06d
Code-Egg <eric780217@gmail.com>
2025-05-22 15:11:47 +0800

    test

--- build.sh
+++ build.sh
@@ -26,10 +26,10 @@ cur_path=$(pwd)
 product_dir=${cur_path}/packaging/build/$product
 result_dir=${product_dir}/$version-$revision/result
 
-build_dir=$(cur_path)/build
-BUILD_SPECS=$(cur_path)/build/SPECS
-BUILD_SOURCES=$(cur_path)/build/SOURCES
-BUILD_SRPMS=$(cur_path)/build/SRPMS
+build_dir=$cur_path/build
+BUILD_SPECS=$cur_path/build/SPECS
+BUILD_SOURCES=$cur_path/build/SOURCES
+BUILD_SRPMS=$cur_path/build/SRPMS
 
 BUILDER_NAME="LiteSpeedTech"
 BUILDER_EMAIL="info@litespeedtech.com"
